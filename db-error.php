3914185aa41b7b55c5077d4b7ba09a5e6e5c5608
Shai Eistein <shai@eistein.net>
2017-10-10 17:05:25 +0300

    uploaded error page

---
+++ db-error.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Something Went Wrong </title>
+    </head>
+    <body dir="rtl" lang="he" background="bg.jpg">
+
+    <marquee>  קרתה תקלה, נא ליצור קשר עם צוות מחשוב.	</marquee>
+	
+	<p align ="center">
+
+    <iframe  width="560" height="315" src="https://www.youtube.com/embed/t3otBjVZzT0" frameborder="0" allowfullscreen></iframe>
+</p>
+
+<p>
+בינתיים אפשר:
+<ul>
+<li> להיכנס ל
+<a href="https://docs.google.com/spreadsheet/ccc?key=0Av-sPifpQV9RdEs3VENUSFVTOFZsZ1owekJBenlhbmc#gid=0">דף קשר</a></li>
+<li> להיכנס ל
+<a href="https://docs.google.com/spreadsheet/ccc?key=0AiMz0_t_pPvFdEhtSDJwN0NPWGJZX2NsTW1JR01rV3c#gid=0">לוח תורנויות</a></li>
+<li>לקנות בקואופ.<br> שעות הפתיחה המעודכנות של הקואופרטיב הן ראשון 19:30-21:30, שני, שלישי, רביעי וחמישי 16:30-21:30 ושישי 08:30-16:30.</li>
+</p>
+
+</body>
+</html>
